14d66f93e8676b5e0e8926f9802ae44b649d923c
Gintare Jurkenaite <locato008@gmail.com>
2013-03-21 12:00:58 +0200

    Sukurem skripta sincos grafikui

--- sincos.m
+++ sincos.m
@@ -2,5 +2,6 @@ function fsincos=sincos(x);
 % Grazina funkcijos:f=sin(x)+cos(x) reiksme
 % Paleidimas:f=sincos(x), kur x vienamtis vektorius
 % Pvz:f=sincos(1.0).
+% Leistinoji sritis (poaibis apibrezimo srities): [-10;10];
 fsincos=sin(x)+cos(x);
 end
