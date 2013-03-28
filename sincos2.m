e25779c6ecbfd1eb136e028fe86dffdd555f37a7
Gintare Jurkenaite <locato008@gmail.com>
2013-03-28 12:27:00 +0200

    Realizavome dvimati MonteCarlo

---
+++ sincos2.m
@@ -0,0 +1,8 @@
+function fsincos2=sincos(x);
+% Grazina funkcijos:f=sin(x(1))+cos(x(2)) reiksme
+% Paleidimas:f=sincos2(x), kur x vienamtis vektorius
+% Pvz:f=sincos2([1.0,1.0]).
+% Leistinoji sritis (poaibis apibrezimo srities): [-10;10]^2; arba
+% ([-10;10] ; [-10;10]).
+fsincos2=sin(x(1))+cos(x(2)); 
+end
