5fa8ca93521a0b4f0fcb8b9a36a7adbdd470d6e7
Gintare Jurkenaite <locato008@gmail.com>
2013-03-28 12:52:15 +0200

    Nubraizeme grafika2

--- grafikas2.m
+++ grafikas2.m
@@ -1,2 +1,3 @@
-x=[-10:0.2:10];% padaryti, kad breztu dvimate funkcija: surf, mesh komandas naudoti
-plot(x,sincos2(x))%plot netinka
+[x,y]=meshgrid(-10:.2:10);% padaryti, kad breztu dvimate funkcija: surf, mesh komandas naudoti
+f=sin(x)+cos(y);
+surf(x,y,f)
